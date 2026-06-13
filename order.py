b341a280d1a50380b2b3e8cd71e8b276e2a1d628
privetf29-dev <privetf29@gmail.com>
2026-06-14 01:11:51 +0600

    Delete order.py

--- order.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class OrderState(StatesGroup):
-    service = State()
-    from_address = State()
-    to_address = State()
-    details = State()
-    phone = State()
